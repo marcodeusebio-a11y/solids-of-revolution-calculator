2cd2f9210e4a4976a50a4b4627c61d92d2cc79c8
Marco D'Eusebio <marco_deusebio@yahoo.com>
2026-05-19 00:31:05 -0700

    Initial Commit

---
+++ Solids of Revolution Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Solids of Revolution Calculator
+//
+//  Created by Marco D’Eusebio on 5/19/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
